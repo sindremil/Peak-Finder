2ba6cceed8cfd88ee48277d7c52564cbc65eb541
leandert <leandert@stud.ntnu.no>
2023-10-05 15:02:39 +0200

    ♻Refactored DestinationCard
    DestinationCard is now more readable and should be easier to maintain.
    Props still need to be added.

--- src/components/DestinationCard.tsx
+++ src/components/DestinationCard.tsx
@@ -21,51 +21,81 @@ export default function DestinationCard() {
     <Container>
       <Card raised>
         <CardActionArea>
-          <CardContent>
-            <Typography variant="h4">Hemsedal</Typography>
-          </CardContent>
-          <CardMedia
-            sx={{ height: 240 }}
-            image={hemsedalImage}
-            title="Hemsedal's Roni chairlift"
-          />
-          <CardContent>
-            <List>
-              <ListItem>
-                <ListItemIcon>
-                  <HeightIcon />
-                </ListItemIcon>
-                <ListItemText primary="1004m (2160m - 3164m)" />
-              </ListItem>
-              <ListItem sx={{ display: "flex", flexWrap: "wrap" }}>
-                <ListItemIcon>
-                  <RouteIcon />
-                </ListItemIcon>
-                <Box sx={{ display: "flex", color: "white" }}>
-                  <ListItemText
-                    primary="35km"
-                    sx={{ background: "blue", padding: "0.5vw 0.25vw" }}
-                  />
-                  <ListItemText
-                    primary="53km"
-                    sx={{ background: "red", padding: "0.5vw 0.25vw" }}
-                  />
-                  <ListItemText
-                    primary="22km"
-                    sx={{ background: "black", padding: "0.5vw 0.25vw" }}
-                  />
-                </Box>
-              </ListItem>
-              <ListItem>
-                <ListItemIcon>
-                  <ArrowOutwardIcon />
-                </ListItemIcon>
-                <ListItemText primary="12 heiser" />
-              </ListItem>
-            </List>
-          </CardContent>
+          <DestinationName />
+          <DestinationImage />
+          <DestinationInfo />
         </CardActionArea>
       </Card>
     </Container>
+  );
+}
+
+//Destination name, image and info
+function DestinationName(): JSX.Element {
+  return(
+    <CardContent>
+      <Typography variant="h4">Hemsedal</Typography>
+    </CardContent>
+  );
+}
+
+function DestinationImage(): JSX.Element {
+  return(
+    <CardMedia
+      sx={{ height: 240 }}
+      image={hemsedalImage}
+      title="Hemsedal's Roni chairlift"
+    />
+  );
+}
+
+function DestinationInfo(): JSX.Element {
+  return(
+    <CardContent>
+      <List>
+        <DestinationHeight />
+        <DestinationPiste />
+        <DestinationLifts />
+      </List>
+    </CardContent>
+  );
+}
+
+
+//List items in the in the card
+function DestinationHeight(): JSX.Element {
+  return (
+    <ListItem>
+      <ListItemIcon>
+        <HeightIcon />
+      </ListItemIcon>
+      <ListItemText primary="1004m (2160m - 3164m)" />
+    </ListItem>
+  );
+}
+
+function DestinationPiste(): JSX.Element {
+  return (
+    <ListItem sx={{display: "flex", flexWrap: "wrap"}}>
+      <ListItemIcon>
+        <RouteIcon />
+      </ListItemIcon>
+      <Box sx={{display: "flex", color: "white"}}>
+        <ListItemText primary="35km" sx={{background: "blue", padding: "0.5vw 0.25vw"}}/>
+        <ListItemText primary="53km"  sx={{background: "red", padding: "0.5vw 0.25vw"}}/>
+        <ListItemText primary="22km"  sx={{background: "black", padding: "0.5vw 0.25vw"}}/>
+      </Box>
+    </ListItem>
+  );
+}
+
+function DestinationLifts(): JSX.Element {
+  return(
+    <ListItem>
+      <ListItemIcon>
+        <ArrowOutwardIcon />
+      </ListItemIcon>
+      <ListItemText primary="12 heiser" />
+    </ListItem>
   );
 }
