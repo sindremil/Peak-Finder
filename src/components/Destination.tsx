cfc0d2980aa3a3b9b301e36f9415300a457de9e7
jesperrg <jesperrg@stud.ntnu.no>
2023-10-06 05:07:07 +0200

    ♻️Added Comments to all funcitons in Destination
    Added Comments to all funcitons in Destination.tsx.
    This commit also contains a different way to display the reviews.
    Personally I think this is better because it seperates the info displayed.
    Changed the TbarLiftIcon.svg to no longer have a black border.
    Changed a letter from capital to lowercase in the review by "John Smith".

--- src/components/Destination.tsx
+++ src/components/Destination.tsx
@@ -17,9 +17,14 @@ import GondolaIcon from "../assets/GondolaIcon.svg";
 import SkiliftIcon from "../assets/SkiliftIcon.svg";
 import TbarLiftIcon from "../assets/TbarLiftIcon.svg";
 
+// This is a temporary constant for the destination img
+// Will be changed later to be a prop and therefore dynamic
 const destinationImgSrc =
   "https://vcdn.bergfex.at/images/resized/75/a6f8bc60ae1e5475_4c61ad8798145fc6@2x.jpg";
 
+// This function takes a single argument children (which is a ReactNode)
+// and returns a JSX element (React component) representing a <Box> with left padding.
+// It wraps the children with a box that has a left padding of 20 pixels.
 function BoxWithPaddingLeft({
   children,
 }: {
@@ -28,21 +33,32 @@ function BoxWithPaddingLeft({
   return <Box sx={{ paddingLeft: "20px" }}>{children}</Box>;
 }
 
-function DestinationImage({ img }: { img: string }): JSX.Element {
-  return (
-    <Box
-      component="img"
-      src={img}
-      alt="Ski Destination"
-      sx={{ maxWidth: "100%" }}
-    />
-  );
+// This function takes two arguments, name and img,
+// and returns a JSX element representing an <img> tag
+// with the specified src and alt attributes.
+// It's used to display an image associated with a destination.
+function DestinationImage({
+  name,
+  img,
+}: {
+  name: string;
+  img: string;
+}): JSX.Element {
+  return <Box component="img" src={img} alt={name} sx={{ maxWidth: "100%" }} />;
 }
 
+// This function takes a single argument name
+// and returns a JSX element representing a <Typography> component
+// with the specified name as the content.
+// It's used to display the name of a destination.
 function DestinationName({ name }: { name: string }): JSX.Element {
   return <Typography variant="h2">{name}</Typography>;
 }
 
+// This function takes a single argument country
+// and returns a JSX element representing a <ListItem>
+// with the primary text "Country" and the country as secondary text.
+// It's used to display the country of a destination.
 function DestinationCountry({ country }: { country: string }): JSX.Element {
   return (
     <ListItem>
@@ -51,6 +67,11 @@ function DestinationCountry({ country }: { country: string }): JSX.Element {
   );
 }
 
+// This function takes two arguments, minHeight and maxHeight,
+// and returns a JSX element representing a <ListItem>
+// with the primary text "Height" and a string showing the range of heights
+// (e.g., "1000 m - 3300 m (2300 m)") as secondary text.
+// It's used to display the height range of a destination.
 function DestinationHeight({
   minHeight,
   maxHeight,
@@ -69,6 +90,9 @@ function DestinationHeight({
   );
 }
 
+// This function takes three arguments, country, minHeight, and maxHeight,
+// and returns a JSX element that contains a list of destination information,
+// including country and height, using the previously defined functions.
 function DestinationInfo({
   country,
   minHeight,
@@ -86,6 +110,11 @@ function DestinationInfo({
   );
 }
 
+// This function takes four arguments, trackType, trackDistance, TrackIcon, and iconColor,
+// and returns a JSX element representing a <ListItem>
+// with an icon (specified by TrackIcon),
+// the trackType as primary text, and the trackDistance as secondary text.
+// It's used to display information about ski tracks.
 function DestinationTrack({
   trackType,
   trackDistance,
@@ -109,6 +138,10 @@ function DestinationTrack({
   );
 }
 
+// This function takes three arguments, blue, red, and black,
+// and returns a JSX element that displays a list of ski tracks
+// of different difficulty levels (beginner, intermediate, difficult)
+// using the previously defined DestinationTrack function.
 function DestinationTracks({
   blue,
   red,
@@ -147,6 +180,11 @@ function DestinationTracks({
   );
 }
 
+// This function takes three arguments, liftType, liftAmount, and liftIcon,
+// and returns a JSX element representing a <ListItem>
+// with an icon (specified by liftIcon),
+// the liftType as primary text, and the liftAmount as secondary text.
+// It's used to display information about ski lifts.
 function DestinationLift({
   liftType,
   liftAmount,
@@ -170,6 +208,10 @@ function DestinationLift({
   );
 }
 
+// This function takes three arguments, gondolas, chairlifts, and tbarLifts,
+// and returns a JSX element that displays a list of different types of ski lifts
+// (e.g., gondolas, chairlifts, T-bar lifts)
+// using the previously defined DestinationLift function.
 function DestinationLifts({
   gondolas,
   chairlifts,
@@ -205,6 +247,10 @@ function DestinationLifts({
   );
 }
 
+// This function takes two arguments, passType and passPrice,
+// and returns a JSX element representing a <ListItem>
+// with the passType as primary text and the passPrice as secondary text.
+// It's used to display information about ski pass prices.
 function DestinationPass({
   passType,
   passPrice,
@@ -219,6 +265,9 @@ function DestinationPass({
   );
 }
 
+// This function takes two arguments, adult and youth,
+// and returns a JSX element that displays a list of ski pass prices
+// for both adults and youth using the previously defined DestinationPass function.
 function DestinationPrices({
   adult,
   youth,
@@ -239,6 +288,10 @@ function DestinationPrices({
   );
 }
 
+// This function takes two arguments, reviewer and comment,
+// and returns a JSX element representing a <ListItem>
+// with the reviewer as primary text and the comment as secondary text.
+// It's used to display user reviews for the destination.
 function DestinationReview({
   reviewer,
   comment,
@@ -257,6 +310,9 @@ type DestinationReviewsProps = {
   reviewList: { username: string; comment: string }[];
 };
 
+// This function takes an array of review objects as reviewList
+// and returns a JSX element that displays a list of user reviews
+// using the previously defined DestinationReview function.
 function DestinationReviews({
   reviewList,
 }: DestinationReviewsProps): JSX.Element {
@@ -277,6 +333,12 @@ function DestinationReviews({
   );
 }
 
+// This is the main component exported from the module.
+// It takes a single argument destinationName
+// and returns a JSX element representing a complete view of destination information.
+// It uses the previously defined functions to display information
+// about the destination, including
+// images, name, country, height, ski tracks, ski lifts, ski pass prices, and user reviews.
 export default function Destination({
   destinationName,
 }: {
@@ -285,7 +347,7 @@ export default function Destination({
   const reviews = [
     {
       username: "John Smith",
-      comment: "Great Place, would love to return here some day.",
+      comment: "Great place, would love to return here some day.",
     },
     {
       username: "Ola Nordmann",
@@ -294,8 +356,8 @@ export default function Destination({
   ];
   return (
     <Box>
-      <Paper elevation={3}>
-        <DestinationImage img={destinationImgSrc} />
+      <Paper elevation={3} sx={{ marginBottom: "20px" }}>
+        <DestinationImage name={destinationName} img={destinationImgSrc} />
         <BoxWithPaddingLeft>
           <DestinationName name={destinationName} />
         </BoxWithPaddingLeft>
@@ -311,9 +373,9 @@ export default function Destination({
         <BoxWithPaddingLeft>
           <DestinationPrices adult={50} youth={30} />
         </BoxWithPaddingLeft>
-        <BoxWithPaddingLeft>
-          <DestinationReviews reviewList={reviews} />
-        </BoxWithPaddingLeft>
+      </Paper>
+      <Paper elevation={3} sx={{ padding: "20px" }}>
+        <DestinationReviews reviewList={reviews} />
       </Paper>
     </Box>
   );
