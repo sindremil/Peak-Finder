3453ea2cb60a473634eee7cb7d57d969180400ea
sindreeh <sindreeh@stud.ntnu.no>
2023-10-10 18:21:05 +0200

    ✨Added mock search functionality to search bar
    Search bar redirects user to results page no matter the input. Routing was altered.

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,67 @@
-// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-// import DestinationPage from "./pages/DestinationPage";
-// import Navbar from "./components/Navbar";
-// import SetPageTitle from "./utils/SetPageTitle";
-// import Result from "./pages/Result";
-// import LoginPage from "./pages/LoginPage";
-// import CreateUserPage from "./pages/CreateUserPage";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import DestinationPage from "./pages/DestinationPage";
+import Navbar from "./components/Navbar";
+import SetPageTitle from "./utils/SetPageTitle";
+import Result from "./pages/Result";
+import LoginPage from "./pages/LoginPage";
+import CreateUserPage from "./pages/CreateUserPage";
 import HomePage from "./pages/HomePage";
 
 function App() {
   return (
-    <HomePage />
-    // <Router basename="/project2">
-    //   <Navbar />
-    //   <Routes>
-    //     <Route
-    //       path="/"
-    //       element={
-    //         <>
-    //           <SetPageTitle title="Results" />
-    //           <Result />
-    //         </>
-    //       }
-    //     />
-    //     <Route
-    //       path="/destination"
-    //       element={
-    //         <>
-    //           <SetPageTitle title="Destination" />
-    //           <DestinationPage />
-    //         </>
-    //       }
-    //     />
-    //     <Route
-    //       path="/login"
-    //       element={
-    //         <>
-    //           <SetPageTitle title="Login" />
-    //           <LoginPage />
-    //         </>
-    //       }
-    //     />
-    //     <Route
-    //       path="/create-user"
-    //       element={
-    //         <>
-    //           <SetPageTitle title="Create User" />
-    //           <CreateUserPage />
-    //         </>
-    //       }
-    //     />
-    //   </Routes>
-    // </Router>
+    <Router basename="/project2">
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <SetPageTitle title="Home" />
+              <HomePage />
+            </>
+          }
+        />
+        <Route
+          path="/results"
+          element={
+            <>
+              <SetPageTitle title="Results" />
+              <Navbar />
+              <Result />
+            </>
+          }
+        />
+        <Route
+          path="/destination"
+          element={
+            <>
+              <SetPageTitle title="Destination" />
+              <Navbar />
+              <DestinationPage />
+            </>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <>
+              <SetPageTitle title="Login" />
+              <Navbar />
+              <LoginPage />
+            </>
+          }
+        />
+        <Route
+          path="/create-user"
+          element={
+            <>
+              <SetPageTitle title="Create User" />
+              <Navbar />
+              <CreateUserPage />
+            </>
+          }
+        />
+      </Routes>
+    </Router>
   );
 }
 
--- src/components/Searchbar.tsx
+++ src/components/Searchbar.tsx
@@ -1,7 +1,24 @@
 import { Box, TextField, Button } from "@mui/material";
 import { Search } from "@mui/icons-material";
+import { useNavigate } from "react-router-dom";
 
 export default function Searchbar() {
+  // Hook used for search bar navigation
+  // State for query will be added when functionality is implemented in future deliverables
+  const navigate = useNavigate();
+
+  // Navigate to results page when either button is pushed, or 'enter' is pressed
+  const handleSearch = () => {
+    navigate("/results");
+  };
+
+  // Call handleSearch when 'enter' is pressed
+  const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") {
+      handleSearch();
+    }
+  };
+
   return (
     <Box
       sx={{
@@ -13,12 +30,13 @@ export default function Searchbar() {
     >
       <TextField
         placeholder="Destinasjon, land..."
+        onKeyDown={handleKeyPress}
         sx={{
           flex: 1,
           borderColor: "#2074d4",
         }}
       />
-      <Button variant="contained">
+      <Button variant="contained" onClick={handleSearch}>
         <Search />
       </Button>
     </Box>
