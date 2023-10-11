70d4024db92e21fea722d0816d4750cb22ed0f64
leandert <leandert@stud.ntnu.no>
2023-10-11 22:29:59 +0200

    🎨Added mock data for 8 destinations

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -1,7 +1,7 @@
 import { Breadcrumbs, Container, Grid, Link, Typography } from "@mui/material";
 import DestinationCard from "../components/DestinationCard";
 import DestinationCardProps from "../interfaces/DestinationCardProps";
-import hemsedalImage from "../assets/hemsedal.jpg";
+import { hemsedal, saalbach, zermatt, chamonix, verbier, davos, courchevel, innsbruck, stMoritz } from "../mockData/destinations";
 
 function addResult(destinationCardProps: DestinationCardProps): JSX.Element {
   return (
@@ -26,32 +26,20 @@ function ResultsBreadCrumbs(): JSX.Element {
 }
 
 export default function Result(): JSX.Element {
-  // Mock values
-  const mockDestinationCardProps: DestinationCardProps = {
-    name: "Saalbach-Hinterglem-Leogang-Fieberbrunn",
-    imageSrc: hemsedalImage,
-    imageAlt: "Bilde av Saalbach-Hinterglem-Leogang-Fieberbrunn",
-    lowestPoint: 1030,
-    highestPoint: 2479,
-    beginner: 130,
-    intermediate: 220,
-    advanced: 80,
-    lifts: 15,
-  };
   return (
     <Container sx={{ marginTop: "2rem" }}>
       <Grid container spacing={4}>
         <ResultsBreadCrumbs />
         {/* Temporary since we cannot implement this without backend */}
-        {addResult(mockDestinationCardProps)}
-        {addResult(mockDestinationCardProps)}
-        {addResult(mockDestinationCardProps)}
-        {addResult(mockDestinationCardProps)}
-        {addResult(mockDestinationCardProps)}
-        {addResult(mockDestinationCardProps)}
-        {addResult(mockDestinationCardProps)}
-        {addResult(mockDestinationCardProps)}
-        {addResult(mockDestinationCardProps)}
+        {addResult(hemsedal)}
+        {addResult(saalbach)}
+        {addResult(chamonix)}
+        {addResult(stMoritz)}
+        {addResult(zermatt)}
+        {addResult(verbier)}
+        {addResult(davos)}
+        {addResult(courchevel)}
+        {addResult(innsbruck)}
       </Grid>
     </Container>
   );
