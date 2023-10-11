cb0366cd6186457498628c0350dcf9ea0e945df5
sindreeh <sindreeh@stud.ntnu.no>
2023-10-11 19:42:15 +0200

    ♻️Refactored search field for DRY code
    A new component ResponsiveTextField now renders TextField with different styling depending on the screen size.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -39,15 +39,16 @@ function Logo({ logoPath }: LogoProps): JSX.Element {
   );
 }
 
-interface SearchFieldProps {
+interface ResponsiveTextFieldProps {
   isDesktop: boolean;
+  setIsDrawerOpen: (open: boolean) => void;
 }
 
-function SearchField({ isDesktop }: SearchFieldProps): JSX.Element {
-  // State is set to true when user presses the search icon on the mobile version
-  // State is set to false when user clicks outside the drawer or the escape key
-  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
-
+// This component is a searchfield that has different behavior depending on the screen size
+function ResponsiveTextField({
+  isDesktop,
+  setIsDrawerOpen,
+}: ResponsiveTextFieldProps): JSX.Element {
   // Query state is currently only used to empty the search field
   // Will be used more in future deliverables when search functionality is implemented
   const [query, setQuery] = useState("");
@@ -61,8 +62,7 @@ function SearchField({ isDesktop }: SearchFieldProps): JSX.Element {
   const navigate = useNavigate();
 
   // Navigate to results page when user presses 'Enter' in search field
-  // The search drawer on mobile is closed
-  // The search field for desktop is emptied
+  // The search drawer on mobile is closed and the searchfield is emptied
   const handleSearch = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key === "Enter") {
       event.preventDefault();
@@ -72,40 +72,55 @@ function SearchField({ isDesktop }: SearchFieldProps): JSX.Element {
     }
   };
 
-  // JSX returned when on desktop
-  if (isDesktop) {
-    return (
-      <TextField
-        placeholder="Destinasjon, land..."
-        variant="standard"
-        onKeyDown={handleSearch}
-        onChange={handleQueryChange}
-        value={query}
-        size="small"
-        sx={{
-          marginTop: 0.7,
-        }}
-        InputProps={{
-          startAdornment: (
-            <InputAdornment
-              position="start"
-              sx={{
-                color: "white",
-              }}
-            >
-              <Search />
-            </InputAdornment>
-          ),
-          style: { color: "white" },
-        }}
-      />
-    );
-  }
-
-  // JSX returned when on mobile
   return (
+    <TextField
+      placeholder="Destinasjon, land..."
+      variant="standard"
+      onKeyDown={handleSearch}
+      onChange={handleQueryChange}
+      value={query}
+      size="small"
+      sx={{
+        marginTop: isDesktop ? 0.7 : undefined,
+        padding: isDesktop ? undefined : 2,
+      }}
+      InputProps={{
+        inputProps: { "aria-label": "Søkefelt" },
+        startAdornment: (
+          <InputAdornment
+            position="start"
+            sx={{
+              color: isDesktop ? "white" : "black",
+            }}
+          >
+            <Search />
+          </InputAdornment>
+        ),
+        style: { color: isDesktop ? "white" : "black" },
+      }}
+    />
+  );
+}
+
+interface SearchFieldProps {
+  isDesktop: boolean;
+}
+
+function SearchField({ isDesktop }: SearchFieldProps): JSX.Element {
+  // State is set to true when user presses the search icon on the mobile version
+  // State is set to false when user clicks outside the drawer or the escape key
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  // Returns ResponsiveTextField based on screen size
+  return isDesktop ? (
+    <ResponsiveTextField
+      isDesktop={isDesktop}
+      setIsDrawerOpen={setIsDrawerOpen}
+    />
+  ) : (
     <>
       <IconButton
+        aria-label="Søk"
         color="inherit"
         sx={{ paddingRight: 0 }}
         onClick={() => setIsDrawerOpen(true)}
@@ -117,29 +132,9 @@ function SearchField({ isDesktop }: SearchFieldProps): JSX.Element {
         open={isDrawerOpen}
         onClose={() => setIsDrawerOpen(false)}
       >
-        <TextField
-          placeholder="Destinasjon, land..."
-          variant="outlined"
-          onKeyDown={handleSearch}
-          onChange={handleQueryChange}
-          value={query}
-          size="small"
-          sx={{ padding: 2 }}
-          InputProps={{
-            inputProps: {
-              "aria-label": "Søkefelt",
-            },
-            startAdornment: (
-              <InputAdornment
-                position="start"
-                sx={{
-                  color: "black",
-                }}
-              >
-                <Search />
-              </InputAdornment>
-            ),
-          }}
+        <ResponsiveTextField
+          isDesktop={isDesktop}
+          setIsDrawerOpen={setIsDrawerOpen}
         />
       </Drawer>
     </>
