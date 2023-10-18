bbebef9974a8fe241cf167afb2f3165437b310fe
leandert <leandert@stud.ntnu.no>
2023-10-18 16:45:13 +0200

    👷‍♀️ Added Redux to project
    Defined some simplied version of redux hooks in hooks.ts as recommened
    in official docs. Also wrapped storage provider around root.

--- src/main.tsx
+++ src/main.tsx
@@ -7,13 +7,17 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import themeConfig from "./utils/themeConfig";
+import { Provider } from "react-redux";
+import store from "./store";
 
 const theme = createTheme(themeConfig);
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <Provider store={store}>
     <CssBaseline />
     <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
+    </Provider>
   </React.StrictMode>,
 );
