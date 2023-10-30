2554eb751d48c9ed535fb8be69acf78c048bb2ec
leandert <leandert@stud.ntnu.no>
2023-10-30 17:05:41 +0100

    👷‍♀️Added tanstack query to project

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,9 +1,11 @@
 module.exports = {
+  plugins: ["@tanstack/query"],
   extends: [
     "airbnb",
     "airbnb/hooks",
     "airbnb-typescript",
     "eslint-config-prettier",
+    "plugin:@tanstack/eslint-plugin-query/recommended",
   ],
   parserOptions: {
     project: ["./tsconfig.json", "./tsconfig.node.json"],
