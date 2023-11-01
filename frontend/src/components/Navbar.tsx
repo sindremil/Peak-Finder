d4f1784f226815a2d7c558569fed581017c97816
sindreeh <sindreeh@stud.ntnu.no>
2023-11-01 17:34:42 +0100

    🔥Removed searchfield in Navbar for simple icon
    Navbar now contains a simple search button that routes the user home, instead of the responsive textfield.

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,15 +1,7 @@
-import {
-  AppBar,
-  Toolbar,
-  Box,
-  IconButton,
-  TextField,
-  InputAdornment,
-  Drawer,
-} from "@mui/material";
+import { AppBar, Toolbar, Box, IconButton } from "@mui/material";
 import { useEffect, useState } from "react";
 import { Search } from "@mui/icons-material";
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import logo from "../assets/logos/logo-no-background.svg";
 import logoCropped from "../assets/logos/logo-no-background-cropped.svg";
 
@@ -37,123 +29,28 @@ function Logo({ logoPath }: LogoProps): JSX.Element {
   );
 }
 
-interface ResponsiveTextFieldProps {
-  isDesktop: boolean;
-  setIsDrawerOpen: (open: boolean) => void;
-}
-
-// This component is a searchfield that has different behavior depending on the screen size
-function ResponsiveTextField({
-  isDesktop,
-  setIsDrawerOpen,
-}: ResponsiveTextFieldProps): JSX.Element {
-  // Query state is currently only used to empty the search field
-  // Will be used more in future deliverables when search functionality is implemented
-  const [query, setQuery] = useState("");
-
-  // Function for handling query change
-  const handleQueryChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setQuery(event.target.value);
-  };
-
-  // Hook used for search bar navigation
-  const navigate = useNavigate();
-
-  // Navigate to results page when user presses 'Enter' in search field
-  // The search drawer on mobile is closed and the searchfield is emptied
-  const handleSearch = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === "Enter") {
-      event.preventDefault();
-      setIsDrawerOpen(false);
-      setQuery("");
-      navigate("/results");
-    }
-  };
-
+function SearchIcon(): JSX.Element {
   return (
-    <TextField
-      placeholder="Destinasjon, land..."
-      variant="standard"
-      onKeyDown={handleSearch}
-      onChange={handleQueryChange}
-      value={query}
-      size="small"
-      sx={{
-        marginTop: isDesktop ? 0.7 : undefined,
-        padding: isDesktop ? undefined : 2,
-      }}
-      InputProps={{
-        inputProps: { "aria-label": "Søkefelt" },
-        startAdornment: (
-          <InputAdornment
-            position="start"
-            sx={{
-              color: isDesktop ? "white" : "black",
-            }}
-          >
-            <Search />
-          </InputAdornment>
-        ),
-        style: { color: isDesktop ? "white" : "black" },
-      }}
-    />
-  );
-}
-
-interface SearchFieldProps {
-  isDesktop: boolean;
-}
-
-function SearchField({ isDesktop }: SearchFieldProps): JSX.Element {
-  // State is set to true when user presses the search icon on the mobile version
-  // State is set to false when user clicks outside the drawer or the escape key
-  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
-
-  // Returns ResponsiveTextField based on screen size
-  return isDesktop ? (
-    <ResponsiveTextField
-      isDesktop={isDesktop}
-      setIsDrawerOpen={setIsDrawerOpen}
-    />
-  ) : (
-    <>
-      <IconButton
-        aria-label="Søk"
-        color="inherit"
-        sx={{ paddingRight: 0 }}
-        onClick={() => setIsDrawerOpen(true)}
-      >
+    <Box component={Link} to="/" sx={{ color: "white" }}>
+      <IconButton aria-label="Søk" color="inherit">
         <Search sx={{ ...iconSize }} />
       </IconButton>
-      <Drawer
-        anchor="top"
-        open={isDrawerOpen}
-        onClose={() => setIsDrawerOpen(false)}
-      >
-        <ResponsiveTextField
-          isDesktop={isDesktop}
-          setIsDrawerOpen={setIsDrawerOpen}
-        />
-      </Drawer>
-    </>
+    </Box>
   );
 }
 
 export default function Navbar(): JSX.Element {
   // State for changing logo based on width
   const [isLogoCropped, setIsLogoCropped] = useState(false);
-  // State for changing search field based on width
-  const [isSearchFieldVisible, setIsSearchFieldVisible] = useState(true);
 
-  // Effect for handling logo and search field change
+  // Effect for handling logo change
   useEffect(() => {
-    // Checks width and sets state variables accordingly
+    // Checks width and sets state accordingly
     const handleResize = () => {
       setIsLogoCropped(window.innerWidth < 600);
-      setIsSearchFieldVisible(window.innerWidth >= 800);
     };
 
-    // handleResize function is each time window gets resized
+    // handleResize function is called each time window gets resized
     window.addEventListener("resize", handleResize);
 
     // Initial call to handleResize
@@ -180,7 +77,7 @@ export default function Navbar(): JSX.Element {
             marginRight: 0,
           }}
         >
-          <SearchField isDesktop={isSearchFieldVisible} />
+          <SearchIcon />
         </Box>
       </Toolbar>
     </AppBar>
