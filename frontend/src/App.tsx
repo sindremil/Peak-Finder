95fdf1476a2986648fdffdfe3d024ee47bd821b9
sindreeh <sindreeh@stud.ntnu.no>
2023-11-01 16:52:16 +0100

    ✨Destination page now takes props
    Destination page takes props, and routing is dynamic.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,8 +4,32 @@ import Navbar from "./components/Navbar";
 import SetPageTitle from "./utils/SetPageTitle";
 import Result from "./pages/ResultPage";
 import HomePage from "./pages/HomePage";
+import DestinationProps from "./interfaces/DestinationProps";
+import fischbach from "./assets/Fischbach.jpg";
+
+
+function getDestinationProps(): DestinationProps {
+  return {
+    destinationName: "Hemsedal",
+    destinationImage: fischbach,
+    country: "Norge",
+    minHeight: 1000,
+    maxHeight: 3300,
+    passPrice: 500,
+    beginner: 8,
+    intermediate: 7,
+    advanced: 6,
+    gondolas: 2,
+    chairlifts: 3,
+    surfaceLifts: 4,
+    snowPark: true,
+    nightSki: false,
+    certifed: true,
+  };
+}
 
 function App() {
+  const destinationProps = getDestinationProps();
   return (
     <Router basename="/project2">
       <Routes>
@@ -29,12 +53,12 @@ function App() {
           }
         />
         <Route
-          path="/destination"
+          path={`/${destinationProps.destinationName}`}
           element={
             <>
-              <SetPageTitle title="Destinasjon " />
+              <SetPageTitle title={destinationProps.destinationName} />
               <Navbar />
-              <DestinationPage />
+              <DestinationPage destinationProps={destinationProps}/>
             </>
           }
         />
