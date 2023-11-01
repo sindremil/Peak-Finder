d71c524a885d81d5f2471d6cf4ca65209d721721
jesperrg <jesperrg@stud.ntnu.no>
2023-11-01 14:53:19 +0100

    ♻️Reinstated File Content That Was Missing

---
+++ frontend/src/components/Filter/filterSlice.ts
@@ -0,0 +1,86 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+interface FilterState {
+  hasPark: boolean;
+  hasNightSkiing: boolean;
+  hasChairlift: boolean;
+  hasGondola: boolean;
+  isCertified: boolean;
+
+  minElevationDifference: number;
+  minBaseElevation: number;
+  minTotalPiste: number;
+  minTotalLifts: number;
+  maxDayPassPrice: number;
+}
+
+const initialState: FilterState = {
+  hasPark: false,
+  hasNightSkiing: false,
+  hasChairlift: false,
+  hasGondola: false,
+  isCertified: false,
+
+  minElevationDifference: 0,
+  minBaseElevation: 0,
+  minTotalPiste: 0,
+  minTotalLifts: 0,
+  maxDayPassPrice: 200,
+};
+
+/* eslint-disable no-param-reassign */
+// no-param-reassign is disabled in this method since it uses immer
+export const filterSlice = createSlice({
+  name: "filter",
+  // createSlice will infer the state type from the `initialState` argument
+  initialState,
+  reducers: {
+    toggleHasPark: (state) => {
+      state.hasPark = !state.hasPark;
+    },
+    toggleHasNightSkiing: (state) => {
+      state.hasNightSkiing = !state.hasNightSkiing;
+    },
+    toggleHasChairlift: (state) => {
+      state.hasChairlift = !state.hasChairlift;
+    },
+    toggleHasGondola: (state) => {
+      state.hasGondola = !state.hasGondola;
+    },
+    toggleIsCertified: (state) => {
+      state.isCertified = !state.isCertified;
+    },
+    setMinElevationDifference: (state, action) => {
+      state.minElevationDifference = action.payload;
+    },
+    setMinBaseElevation: (state, action) => {
+      state.minBaseElevation = action.payload;
+    },
+    setMinTotalPiste: (state, action) => {
+      state.minTotalPiste = action.payload;
+    },
+    setMinTotalLifts: (state, action) => {
+      state.minTotalLifts = action.payload;
+    },
+    setMaxDayPassPrice: (state, action) => {
+      state.maxDayPassPrice = action.payload;
+    },
+  },
+});
+/* eslint-enable no-param-reassign */
+
+export const {
+  toggleHasPark,
+  toggleHasNightSkiing,
+  toggleHasChairlift,
+  toggleHasGondola,
+  toggleIsCertified,
+
+  setMinElevationDifference,
+  setMinBaseElevation,
+  setMinTotalPiste,
+  setMinTotalLifts,
+  setMaxDayPassPrice,
+} = filterSlice.actions;
+
+export default filterSlice.reducer;
