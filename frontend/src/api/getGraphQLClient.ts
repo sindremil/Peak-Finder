4d9388fdb90a10548cd983010249e25666fa0a9f
leandert <leandert@stud.ntnu.no>
2023-11-04 11:22:41 +0100

    ✨Created singleton getter for GraphQLClient

---
+++ frontend/src/api/getGraphQLClient.ts
@@ -0,0 +1,13 @@
+import { GraphQLClient } from "graphql-request";
+import { backendEndpoint } from "../config";
+
+let clientInstance: GraphQLClient | null = null;
+
+// Singleton pattern used to avoid multiple clients
+export default function getGraphQLClient(): GraphQLClient {
+  if (clientInstance === null) {
+    clientInstance = new GraphQLClient(backendEndpoint);
+  }
+
+  return clientInstance
+}
