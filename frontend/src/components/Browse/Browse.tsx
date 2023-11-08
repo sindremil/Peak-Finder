8c2cc86f04066eb1356e317126dbe38d369a4b27
jesperrg <jesperrg@stud.ntnu.no>
2023-11-08 19:29:28 +0100

    🐛InputLabel is Now Correctly Hidden
    The InputLabel in Browse now hides when the SearchbarList is displayed.

--- frontend/src/components/Browse/Browse.tsx
+++ frontend/src/components/Browse/Browse.tsx
@@ -1,17 +1,30 @@
-import { Select, FormControl, FormHelperText, InputLabel } from "@mui/material";
+import {
+  FormControl,
+  FormHelperText,
+  InputLabel,
+  List,
+  Select,
+} from "@mui/material";
+import { useSelector } from "react-redux";
 import BrowseList from "./BrowseList";
 
 export default function Browse(): JSX.Element {
+  const sok = useSelector((state: any) => state.search.searchTerm);
   return (
     <FormControl sx={{ m: 1, minWidth: 200 }}>
-      <InputLabel id="browseSelectLabel">Land</InputLabel>
+      {sok === "" && (
+        <InputLabel id="browseSelectLabel" sx={{}}>
+          Land
+        </InputLabel>
+      )}
+
       <Select
         labelId="browseSelectLabel"
         id="browseSelect"
         label="Land"
         value=""
       >
-        {BrowseList()}
+        <List>{BrowseList()}</List>
       </Select>
       <FormHelperText>Utforsk basert på land</FormHelperText>
     </FormControl>
