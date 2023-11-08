470ff47a7c7ef058e676bbc59ff36a4d92bef3b8
jesperrg <jesperrg@stud.ntnu.no>
2023-11-08 18:07:54 +0100

    ✨Implemented Custom BreadCrumb Names
    Breadcrumbs now uses the country or destinations name
    instead of just a placeholder "Resultater" or "Destinasjon".

--- frontend/src/components/BreadCrumbs/BreadCrumbs.tsx
+++ frontend/src/components/BreadCrumbs/BreadCrumbs.tsx
@@ -4,28 +4,43 @@ import { Link } from "react-router-dom";
 // Return different Breadcrumbs based on whether the user
 // is on the Results page or the Destination page and
 // whether the Destination page was routed through Results page or search
-export default function BreadCrumbs(
-  isFromResult?: boolean,
-  country?: string,
-): JSX.Element {
+interface BreadCrumbsProps {
+  country?: string;
+  destination?: string;
+  isResult?: boolean;
+  isFromResult?: boolean;
+}
+
+export default function BreadCrumbs({
+  country,
+  destination,
+  isResult,
+  isFromResult,
+}: BreadCrumbsProps): JSX.Element {
   return (
     <Breadcrumbs aria-label="Navigasjon">
       <MuiLink component={Link} underline="hover" color="inherit" to="/">
         Hjem
       </MuiLink>
-      {country && isFromResult && (
+      {!isResult && isFromResult && (
         <MuiLink
           component={Link}
           underline="hover"
           color="inherit"
-          to={`/results/${encodeURIComponent(country)}`}
+          to={`/results/${encodeURIComponent(country || "")}`}
         >
-          Resultater
+          {country}
         </MuiLink>
       )}
-      {(country && (
-        <Typography color="text.primary">Destinasjon</Typography>
-      )) ?? <Typography color="text.primary">Resultater</Typography>}
+      {!isResult && <Typography color="text.primary">{destination}</Typography>}
+      {isResult && <Typography color="text.primary">{country}</Typography>}
     </Breadcrumbs>
   );
 }
+
+BreadCrumbs.defaultProps = {
+  country: "",
+  destination: "",
+  isResult: false,
+  isFromResult: false,
+} as BreadCrumbsProps;
