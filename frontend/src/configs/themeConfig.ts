2700f001524a2bda6daeb7e471446ae69fd6f098
jesperrg <jesperrg@stud.ntnu.no>
2023-11-28 20:34:40 +0100

    ✨Implemented dark mode
    Added a redux state called theme to remember what mode is used.
    Moved Themeprovider and CssBaseline into App.
    Added a icon to change the theme state to the Navbar.

--- frontend/src/configs/themeConfig.ts
+++ frontend/src/configs/themeConfig.ts
@@ -1,4 +1,9 @@
-const themeConfig = {
+import { ThemeOptions } from "@mui/material";
+
+const lightThemeConfig: ThemeOptions = {
+  palette: {
+    mode: "light",
+  },
   breakpoints: {
     values: {
       xs: 0,
@@ -10,4 +15,19 @@ const themeConfig = {
   },
 };
 
-export default themeConfig;
+const darkThemeConfig: ThemeOptions = {
+  palette: {
+    mode: "dark",
+  },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 720,
+      md: 960,
+      lg: 1280,
+      xl: 1920,
+    },
+  },
+};
+
+export { lightThemeConfig, darkThemeConfig };
